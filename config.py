038da4045bb2252569fce89f92eac1b5e7cca14a
Xingxing000 <56240291+Xingxing000@users.noreply.github.com>
2022-04-15 13:02:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c8lbzaiol1nx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "password"
+customdb = "employee"
+custombucket = "xinghang-employee"
+customregion = "us-east-1"
